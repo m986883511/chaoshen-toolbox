899e3c3acad7620bac13d62c4d1a565c2d375c93
wang.chao <wang.chao@astute-tec.com>
2023-05-31 09:57:35 +0800

    优化help显示

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # chaoshen-toolbox
 这是我为了日常快速开发写的一些脚本，这是将来成为大佬的小小见证
+
+
+
+快速安装
+```shell
+pip install git+https://github.com/m986883511/chaoshen-toolbox.git
+
+```
--- chaoshen_toolbox/scripts/openstack/cs_openstack_log_delete_req.py
+++ chaoshen_toolbox/scripts/openstack/cs_openstack_log_delete_req.py
@@ -5,8 +5,10 @@ import re
 from chaoshen_toolbox.utils import file
 
 usage = """
-log from this to this, just delete [req-*]\n
+A example like follow, just delete [req-*]
+\u2193  \u2193  \u2193  \u2193  \u2193  \u2193  \u2193  \u2193  \u2193  \u2193
 2023-05-30 16:12:40.658 19059 INFO nova.compute.manager [req-47ce1786-eb8e-4ac8-ac9a-aad38af1400a 90bd088c758b4b0787bf153626d9b91d 9783fe0f8b0549f98528967424c50f2f - default default] [instance: 3409a7cf-16a1-4003-8e07-0db6f08d24c5] Rebuilding instance
+\u2193  \u2193  \u2193  \u2193  \u2193  \u2193  \u2193  \u2193  \u2193  \u2193
 2023-05-30 16:12:40.658 19059 INFO nova.compute.manager Rebuilding instance
 """
 
@@ -26,7 +28,10 @@ def deal_with(file_path):
 
 
 def main():
-    parser = argparse.ArgumentParser(description=f'delete file req and generate newfile\n{usage}')
+    parser = argparse.ArgumentParser(
+        description=f'delete file req and generate newfile\n{usage}',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
     parser.add_argument('-f', "--file", type=str, help="the log file need to deal with")
     args = parser.parse_args()
     deal_with(file_path=args.file)
