c1b590b16de74377ac960255d6605f61947da98b
wang.chao <wang.chao@astute-tec.com>
2023-07-13 21:19:26 +0800

    add filter log

--- chaoshen_toolbox/scripts/openstack/log_delete_req.py
+++ chaoshen_toolbox/scripts/openstack/log_delete_req.py
@@ -2,7 +2,10 @@ import argparse
 import os
 import re
 
-from chaoshen_toolbox.utils import file
+from pydantic import BaseModel
+
+from chaoshen_toolbox.utils import file, common
+from chaoshen_toolbox import LOG
 
 usage = """
 A example like follow, just delete [req-*]
@@ -13,18 +16,58 @@ A example like follow, just delete [req-*]
 """
 
 
-def deal_with(file_path):
-    if not os.path.isfile(file_path):
-        raise Exception(f'file {file_path} not exist')
+class ArgsModel(BaseModel):
+    log_filepath: str
+    connector: str = '--'
+    no_block: str = ""
+    no_req: bool = False
+    no_level: bool = False
 
-    new_filepath = file.add_suffix_to_file(file_path, 'noreq')
+
+def get_new_filepath(args: ArgsModel):
+    all_no_items = common.get_basemodel_class_variable_names(ArgsModel)
+    new_filepath = args.log_filepath
+    for no_item in all_no_items:
+        if not no_item.startswith('no_'):
+            continue
+        if getattr(args, no_item):
+            no_item = no_item.replace('_', '').replace('-', '')
+            LOG.info(f'will add {no_item} to newfile')
+            new_filepath = file.add_suffix_to_file(new_filepath, no_item, connector=args.connector)
+    LOG.info(f"new_filepath is {new_filepath}")
+    return new_filepath
+
+
+def deal_with_args(args: ArgsModel):
+    if not os.path.isfile(args.log_filepath):
+        raise Exception(f'file {args.log_filepath} not exist')
+
     regex = r"\[.*?req-.*?\]"
-    with open(file_path, 'r') as f:
+    with open(args.log_filepath, 'r') as f:
         lines = f.readlines()
+
+    new_filepath = get_new_filepath(args)
+    if new_filepath == args.log_filepath:
+        LOG.warning("new_filepath == args.log_filepath, should do nothing")
+        return
+
     with open(new_filepath, 'w') as f:
+        new_lines = []
         for line in lines:
-            line = re.sub(regex, "", line)
-            f.write(line)
+
+            if args.no_req:
+                line = re.sub(regex, "", line)
+            if args.no_level:
+                pass
+            if args.no_block:
+                line_list = line.split(' ')
+                need_delete_blocks = [int(i) for i in args.no_block.split(',')]
+                for block in need_delete_blocks:
+                    line_list[block] = ''
+                line = ' '.join(line_list).strip()
+            line = re.sub(r'\s+', ' ', line)
+            new_lines.append(line.rstrip()+os.linesep)
+        f.writelines(new_lines)
 
 
 def main():
@@ -33,8 +76,14 @@ def main():
         formatter_class=argparse.RawTextHelpFormatter
     )
     parser.add_argument("log_filepath", type=str, help="the log file need to deal with")
+    parser.add_argument("-c", "--connector", default='--', help="the log file need to deal with")
+    parser.add_argument("--no-req", action="store_true", help="the log file need to deal with")
+    parser.add_argument("--no-level", action="store_true", help="the log file need to deal with")
+    parser.add_argument("--no-block", default="", help="the log file need to deal with")
+
     args = parser.parse_args()
-    deal_with(file_path=args.log_filepath)
+    args = ArgsModel(**vars(args))
+    deal_with_args(args)
 
 
 if __name__ == '__main__':
