3506760a8cf212be442b517278dc3c619a5897dc
wang.chao <wang.chao@astute-tec.com>
2023-07-31 16:03:38 +0800

    permit-root-ssh

---
+++ tools/cs-permit-root-ssh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sed -i '/StrictHostKeyChecking/d' /etc/ssh/ssh_config
+echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+
+sed -i '/PermitRootLogin/d' /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/ssh_config
+sed -i '/PasswordAuthentication/d' /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/ssh_config
+systemctl restart sshd
