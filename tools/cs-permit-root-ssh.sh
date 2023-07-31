aa9ae1d62860717da2bdc33bbbd251edc58711a5
wang.chao <wang.chao@astute-tec.com>
2023-07-31 16:45:27 +0800

    bug: /etc/ssh/sshd_config

--- tools/cs-permit-root-ssh.sh
+++ tools/cs-permit-root-ssh.sh
@@ -3,7 +3,7 @@ sed -i '/StrictHostKeyChecking/d' /etc/ssh/ssh_config
 echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 
 sed -i '/PermitRootLogin/d' /etc/ssh/sshd_config
-echo "PermitRootLogin yes" >> /etc/ssh/ssh_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 sed -i '/PasswordAuthentication/d' /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" >> /etc/ssh/ssh_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 systemctl restart sshd
